aa7dc02ec556f0f97fedc65ff1d4998922ee9301
Artiexli <Artieli007@gmail.com>
2022-05-06 12:09:32 -0700

    Minor tweaks to postprocessing and inference

--- data_processing/postprocess.py
+++ data_processing/postprocess.py
@@ -61,7 +61,7 @@ def generate_output_midi(preprocessed_songs: list, Y_hat: list, X: np.array,
           time_percentage = prediction_row[current_index]
           if round(value) != 0 or round(time_percentage) != 0:
             if value >0.7: value = 127
-            else: value = 0
+            else: value = 1
             control_changes[time_percentage] = (control_num, value)
         return current_index, control_changes
       
